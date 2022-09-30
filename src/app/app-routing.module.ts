29bd90cbee490d8201899906057d7661af3d0198
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2022-09-30 16:21:46 +0200

    Gérer les erreurs 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
 import { BlogpostComponent } from './blogpost/blogpost.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'blog-posts/:id',
     component: BlogpostComponent
+  },
+  {
+    path: '**',
+    component: ErrorpageComponent
   }
 ];
 
