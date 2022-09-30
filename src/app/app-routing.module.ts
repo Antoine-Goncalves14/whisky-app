884a59b23c6aee0d0202c95c3e25f406d8e26a9c
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2022-09-30 15:26:17 +0200

    Accéder à un blog post par son ID depuis le backend Express

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
+import { BlogpostComponent } from './blogpost/blogpost.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: BlogpostListComponent
+  },
+  {
+    path: 'blog-posts/:id',
+    component: BlogpostComponent
   }
 ];
 
--- src/app/blogpost.service.ts
+++ src/app/blogpost.service.ts
@@ -7,13 +7,17 @@ import { Blogpost } from './models/blogpost';
   providedIn: 'root'
 })
 export class BlogpostService {
-  baseUrl = 'http://localhost:3000/api/v1/';
+  baseUrl = 'http://localhost:3000/api/v1/blog-posts';
 
   constructor(
     private httpClient: HttpClient
   ) { }
 
   getBlogposts(): Observable<Blogpost[]> {
-    return this.httpClient.get<Blogpost[]>(`${this.baseUrl}/blog-posts`);
+    return this.httpClient.get<Blogpost[]>(`${this.baseUrl}`);
+  }
+
+  getBlogpostById(id): Observable<Blogpost> {
+    return this.httpClient.get<Blogpost>(`${this.baseUrl}/${id}`);
   }
 }
