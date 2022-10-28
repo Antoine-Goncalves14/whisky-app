ef679be8263893efeded6b628392fb552a360c57
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2022-10-28 16:08:02 +0200

    Component dédié à l'édition d'un blog post (partie TypeScript)

--- src/app/blogpost-edit/blogpost-edit.component.ts
+++ src/app/blogpost-edit/blogpost-edit.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { BlogpostService } from "../blogpost.service";
+import { Blogpost } from "../models/blogpost";
 
 @Component({
-  selector: 'app-blogpost-edit',
-  templateUrl: './blogpost-edit.component.html',
-  styleUrls: ['./blogpost-edit.component.css']
+  selector: "app-blogpost-edit",
+  templateUrl: "./blogpost-edit.component.html",
+  styleUrls: ["./blogpost-edit.component.css"],
 })
 export class BlogpostEditComponent implements OnInit {
+  editForm: FormGroup;
+  blogpostId: string;
+  blogpost: Blogpost;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private blogpostService: BlogpostService,
+    private el: ElementRef,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.blogpostId = this.activatedRoute.snapshot.paramMap.get("id");
+    this.blogpostService.getBlogpostById(this.blogpostId).subscribe(
+      (data) => {
+        this.blogpost = data;
+      },
+      (error) => console.error(error)
+    );
+    this.createForm();
   }
 
+  createForm() {
+    this.editForm = this.fb.group({
+      title: "",
+      subTilte: "",
+      content: "",
+      image: "",
+    });
+  }
+
+  upload() {
+    let inputEl: HTMLInputElement =
+      this.el.nativeElement.querySelector("#image");
+    let fileCount: number = inputEl.files.length;
+    let formData = new FormData();
+
+    if (fileCount > 0) {
+      formData.append("image", inputEl.files.item(0));
+      this.blogpostService.uploadImage(formData).subscribe(
+        (data) => console.log(data),
+        (error) => console.error(error)
+      );
+    }
+  }
 }
