0e0724ab94868149c3b1ee6c5ccdf2271547dae2
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2022-10-28 11:02:06 +0200

    Modifications finales du formulaire

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [],
@@ -15,6 +16,7 @@ import { MatInputModule } from "@angular/material/input";
     MatListModule,
     MatButtonModule,
     MatInputModule,
+    MatFormFieldModule,
   ],
   exports: [
     MatToolbarModule,
@@ -22,6 +24,7 @@ import { MatInputModule } from "@angular/material/input";
     MatListModule,
     MatButtonModule,
     MatInputModule,
+    MatFormFieldModule,
   ],
 })
 export class MaterialModule {}
